107bf2f4dfa0214ec5bd71cc9e8eae26a9b2b29f
Grizzly18 <mz-100@mail.ru>
2022-03-07 16:04:02 +0300

    start

---
+++ static/js/style.js
@@ -0,0 +1,4 @@
+// screen
+// if (window.matchMedia('(min-device-aspect-ratio: 16/9)').matches || window.matchMedia('(min-device-aspect-ratio: 16/10)').matches) {
+//    alert('Media Query Matched!')
+// }
